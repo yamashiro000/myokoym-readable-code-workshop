e4c89409f5090140261cb17442563ef99b3d9077
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-05 12:06:34 +0900

    spec3: name magic number

--- recipe.c
+++ recipe.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#define LINE_BUFFER 1024
+
 int
 main(int argc, char *argv[]) {
   FILE *file;
-  char line[1024];
+  char line[LINE_BUFFER];
   char *file_name = argv[1];
 
   file = fopen(file_name, "r");
@@ -12,7 +14,7 @@ main(int argc, char *argv[]) {
     return 1;
   }
 
-  if (fgets(line, 1024, file) == NULL) {
+  if (fgets(line, LINE_BUFFER, file) == NULL) {
     fprintf(stderr, "ファイルの読み込みに失敗しました: <%s>\n", file_name);
     return 1;
   }
