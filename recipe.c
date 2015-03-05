478578022dec189e03b7e003a4aa64269c8cdd7f
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-03-05 11:51:09 +0900

    spec3: split data file

--- recipe.c
+++ recipe.c
@@ -2,7 +2,14 @@
 
 int
 main(void) {
-  printf("%s\n", "オムライス");
+  FILE *file;
+  char line[1024];
+
+  file = fopen("recipe-data.txt", "r");
+
+  fgets(line, 1024, file);
+
+  printf("%s\n", line);
 
   return 0;
 }
